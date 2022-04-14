9b3c80fa89ef31f253ec95bade60cc3eccd7878e
Karintou <kaitotti0729@gmail.com>
2022-04-14 15:18:38 +0900

    Create sqrt.go

---
+++ Golang practice/sqrt.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func MySqrt(x float64) float64 {
+	z, prev_z := float64(0.1), float64(0)
+	s := float64(0)
+
+	for i := 1; ; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Printf("LOOP:%d => z:%.10f prev_z:%.10f s:%10f\n", i, z, prev_z, s)
+
+		s = prev_z - z
+		if s < 0 {
+			s *= -1
+		}
+
+		if s < 0.00001 {
+			break
+		}
+
+		prev_z = z
+	}
+
+	return z
+}
+
+func main() {
+	n := float64(13)
+
+	fmt.Println("INIT:", n)
+	fmt.Println("MySqrt  -> ", MySqrt(n))
+	fmt.Println("library -> ", math.Sqrt(n))
+}
